b50f36d2d792d8cce8f691468fc20e4481cc3f6e
Audrow Nash <audrow@hey.com>
2022-06-07 16:28:06 -0500

    Create a new branch from the default branch and set it as default
    Signed-off-by: Audrow Nash <audrow@hey.com>

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,73 @@
-import Person from './__types__/Person'
+import 'dotenv/config'
+import {Octokit} from 'octokit'
 
-export function whoIsYounger(personA: Person, personB: Person): Person {
-  return personA.age < personB.age ? personA : personB
+const githubAccessToken = process.env.GITHUB_TOKEN
+const octokit = new Octokit({auth: githubAccessToken})
+
+async function newBranchFromDefaultBranch({
+  owner,
+  repo,
+  newBranchName,
+}: {
+  owner: string
+  repo: string
+  newBranchName: string
+}) {
+  // GET DEFAULT BRANCH
+  const defaultBranch = (
+    await octokit.rest.repos.get({
+      owner,
+      repo,
+    })
+  ).data.default_branch
+  console.log(defaultBranch)
+
+  await createNewBranch({owner, repo, newBranchName, baseBranch: defaultBranch})
+
+  // SET NEW BRANCH AS DEFAULT BRANCH
+  await octokit.rest.repos.update({
+    owner,
+    repo,
+    default_branch: newBranchName,
+  })
 }
 
-if (typeof require !== 'undefined' && require.main === module) {
-  console.log(whoIsYounger({name: 'John', age: 20}, {name: 'Jane', age: 30}))
+async function createNewBranch({
+  owner,
+  repo,
+  baseBranch,
+  newBranchName,
+}: {
+  owner: string
+  repo: string
+  baseBranch: string
+  newBranchName: string
+}) {
+  // GET SHA OF BASE BRANCH
+  const sha1 = (
+    await octokit.rest.git.getRef({
+      owner,
+      repo,
+      ref: `heads/${baseBranch}`,
+    })
+  ).data.object.sha
+  console.log(sha1)
+
+  // MAKE BRANCH FROM DEFAULT BRANCH
+  await octokit.rest.git.createRef({
+    owner,
+    repo,
+    ref: `refs/heads/${newBranchName}`,
+    sha: sha1,
+  })
 }
+
+async function main() {
+  const owner = 'audrow'
+  const repo = 'rclcpp'
+  const newBranchName = 'my-new-branch4'
+
+  await newBranchFromDefaultBranch({owner, repo, newBranchName})
+}
+
+main()
