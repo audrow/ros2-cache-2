fd363ca086c656f16522988757746699779fa7d6
Audrow Nash <audrow@hey.com>
2022-08-16 10:10:26 -0500

    Use a path context
    Signed-off-by: Audrow Nash <audrow@hey.com>

--- test/git.test.mjs
+++ test/git.test.mjs
@@ -3,25 +3,25 @@ import simpleGit from 'simple-git'
 import tempFs from 'temp-fs'
 import {git} from '../dist/main.js'
 
-let path
-
-test.before(async () => {
+test.before(async (t) => {
   const {path: path_} = tempFs.mkdirSync({
     recursive: true,
     track: true,
   })
-  path = path_.toString()
+  t.context.path = path_.toString()
   const repo = 'https://github.com/ros2/common_interfaces'
-  await simpleGit().clone(repo, path)
+  await simpleGit().clone(repo, t.context.path)
 })
 
 test.after.always(async () => {
   tempFs.clearSync()
-  path = ''
 })
 
 test('has branch', async (t) => {
   t.plan(3)
+
+  const path = t.context.path
+
   t.true(await git.hasBranch(path, RegExp('galactic')))
   t.true(await git.hasBranch(path, RegExp('master')))
   t.false(await git.hasBranch(path, RegExp('not-a-branch')))
@@ -29,6 +29,8 @@ test('has branch', async (t) => {
 
 test('create a new branch', async (t) => {
   t.timeout(10000)
+
+  const path = t.context.path
 
   const baseBranch = 'rolling'
   const newBranch = 'my-new-branch'
