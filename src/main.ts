a7bdad948d23fa1eeee42b61dadb0c0ebf460e3d
Audrow Nash <audrow@hey.com>
2022-08-16 10:47:49 -0500

    Remove ping and version
    Signed-off-by: Audrow Nash <audrow@hey.com>

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,7 @@
 export * as git from './git'
 export * as reposFile from './repos-file'
 export {DistributionYamlFile, ProcessedRepos, ReposFile}
-// export * as reposFile from './repos-file'
 
 import type DistributionYamlFile from './__types__/DistributionYamlFile'
 import type ProcessedRepos from './__types__/ProcessedRepos'
 import type ReposFile from './__types__/ReposFile'
-
-const PKG_VERSION = 'PKG_VERSION'
-export const version = PKG_VERSION
-
-export function ping() {
-  return 'pong'
-}
--- test/version.mjs
+++
@@ -1,7 +0,0 @@
-import test from 'ava'
-import { version } from '../dist/main.js'
-import pkg from '../package.json' assert { type: 'json' }
-
-test('version', (t) => {
-  t.is(version, pkg.version)
-})
