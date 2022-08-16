04e8d1b95dc2bff422bd46cb11ff1783b857228d
Audrow Nash <audrow@hey.com>
2022-08-16 10:23:04 -0500

    Update exports
    Signed-off-by: Audrow Nash <audrow@hey.com>

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 export * as git from './git'
+export {DistributionYamlFile, ProcessedRepos, ReposFile}
+// export * as reposFile from './repos-file'
+
+import type DistributionYamlFile from './__types__/DistributionYamlFile'
+import type ProcessedRepos from './__types__/ProcessedRepos'
+import type ReposFile from './__types__/ReposFile'
 
 const PKG_VERSION = 'PKG_VERSION'
 export const version = PKG_VERSION
